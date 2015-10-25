94fee0c82f92927566498bb7fb0541c286916716
Prakhar Bahuguna <me@prakhar.uk>
2015-10-25 21:23:32 +0000

    Brew changes

--- brew.sh
+++ brew.sh
@@ -2,12 +2,6 @@
 
 # Install command-line tools using Homebrew.
 
-# Ask for the administrator password upfront.
-sudo -v
-
-# Keep-alive: update existing `sudo` time stamp until the script has finished.
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
-
 # Make sure we’re using the latest Homebrew.
 brew update
 
@@ -24,11 +18,13 @@ brew install --with-pinentry --with-doc lastpass-cli
 brew install node
 brew install p7zip
 brew install pigz
+brew install --with-docs pixz
 brew install pv
 brew install python3
 brew install rename
 brew install speedtest_cli
 brew install ssh-copy-id
+brew install tmux
 brew install tree
 brew install uncrustify
 
