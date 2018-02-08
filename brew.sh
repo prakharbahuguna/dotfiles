525577a9c46244b181ae34f0e096ad6d4d62e48f
Prakhar Bahuguna <me@prakhar.uk>
2018-02-08 00:02:10 +0000

    Overhauled homebrew script and updated package list

--- brew.sh
+++ brew.sh
@@ -1,45 +1,34 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 # First, install Homebrew if it's not already installed
-command -v brew &> /dev/null || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+command -v brew > /dev/null || ruby -e \
+    "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-# Make sure we’re using the latest Homebrew.
+# Make sure we're using the latest Homebrew.
 brew update
 
 # Upgrade any already-installed formulae.
-brew upgrade
+brew upgrade --cleanup
+brew cask upgrade
 
-# Install other useful binaries.
-brew install axel
-brew install ccache
-brew install ctags
-brew install fzf
-brew install git-lfs
-brew install git-review
-brew install iperf3
-brew install --with-pinentry --with-doc lastpass-cli
-brew install mobile-shell
-brew install neovim
-brew install p7zip
-brew install pigz
-brew install pixz
-brew install pv
-brew install python3
-brew install rename
-brew install --with-wrap-pbcopy-and-pbpaste --with-wrap-launchctl reattach-to-user-namespace
-brew install ripgrep
-brew install speedtest_cli
-brew install ssh-copy-id
-brew install tmux
-brew install tree
-brew install uncrustify
-brew install youtube-dl
+# Package lists
+pkg_list = (axel ccache checkbashisms clang-format ctags fzf git-lfs \
+    git-review iperf3 mosh neovim pixz pv rename reattach-to-user-namespace \
+    ripgrep shellcheck speedtest-cli tmux tree uncrustify)
+cask_list = (anaconda font-hack)
+
+# Install the packages
+brew install ${pkg_list}
+brew cask install ${cask_list}
 
 # Remove outdated versions from the cellar.
 brew cleanup -s
+brew cask cleanup
 
-# Prerequisited for nvim
-pip3 install neovim
-
-# Install FZF bindings
+# Post-install stuff
+git lfs install
 /usr/local/opt/fzf/install
+/usr/local/anaconda3/bin/pip install neovim
+
+# My anaconda don't want none unless you got updated packages, hun
+/usr/local/anaconda3/bin/conda update --all
