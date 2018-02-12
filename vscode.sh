9f90461741696d3168d7c2a7772a200fb8e50282
Prakhar Bahuguna <prakhar.bahuguna@arm.com>
2018-02-12 10:15:47 +0000

    Add VSCode settings and install script

---
+++ vscode.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if command -v code > /dev/null; then
+    if [ "$(uname)" = "Darwin" ]; then
+        VSCODE_DIR="${HOME}/Library/Application Support/Code/User"
+    else
+        VSCODE_DIR="${HOME}/.config/Code"
+    fi
+
+    [ ! -d "${VSCODE_DIR}" ] && mkdir -p "${VSCODE_DIR}"
+    cp settings.json "${VSCODE_DIR}"
+
+    extensions=(MS-DevSkim.vscode-devskim RReverser.llvm eamodio.gitlens \
+                fallenwood.vimL krvajalm.linter-gfortran ms-python.python \
+                ms-vscode.cpptools llvm.tablegen timonwong.shellcheck)
+
+    for ext in "${extensions[@]}"; do
+        code --install-extension "${ext}" || true
+    done
+fi
