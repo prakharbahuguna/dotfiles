3f168c2c37fd648465060d8a22b8a95fcb4469db
Prakhar Bahuguna <prakharbahuguna@users.noreply.github.com>
2020-12-16 17:13:50 +0000

    Updates to homebrew script

--- brew.sh
+++ brew.sh
@@ -1,30 +1,28 @@
 #!/bin/bash
 
 # First, install Homebrew if it's not already installed
-command -v brew > /dev/null || ruby -e \
-    "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+command -v brew > /dev/null ||
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Make sure we're using the latest Homebrew.
 brew update
 
 # Upgrade any already-installed formulae.
 brew upgrade
-brew cask upgrade
 
 # Package lists
-pkg_list=(axel bitwarden-cli ccache checkbashisms clang-format ctags fzf \
-    git-lfs git-review iperf3 neovim pixz pv python \
-    reattach-to-user-namespace rename ripgrep shellcheck speedtest-cli tmux \
-    tree uncrustify youtube-dl)
+pkg_list=(bitwarden-cli ccache checkbashisms clang-format ctags fzf git-lfs \
+    iperf3 neovim python rename ripgrep shellcheck speedtest-cli tmux tree \
+    uncrustify youtube-dl)
 cask_list=(appcleaner coconutbattery docker firefox flycut font-fira-code \
-    gfxcardstatus iterm2 qbittorrent scroll-reverser smcfancontrol spotify \
+    gfxcardstatus iterm2 macs-fan-control qbittorrent scroll-reverser spotify \
     visual-studio-code vlc
 )
 
 # Install the packages
 brew install "${pkg_list[@]}"
-brew tap caskroom/fonts
-brew cask install "${cask_list[@]}"
+brew tap homebrew/cask-fonts
+brew install --cask "${cask_list[@]}"
 
 # Remove outdated versions from the cellar.
 brew cleanup
@@ -33,4 +31,3 @@ brew cleanup
 git lfs install
 /usr/local/opt/fzf/install
 pip3 install -U pip setuptools wheel
-pip3 install neovim
